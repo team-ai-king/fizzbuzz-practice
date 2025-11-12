071b0d5f0bc3f7f330f44ff3cc38cdffa2124951
deayeone <eodusqkqh3@gmail.com>
2025-11-12 15:28:17 +0900

    feat: Print fizzbuzz

---
+++ fizzbuzz.py
@@ -0,0 +1,9 @@
+for n in range(1, 15+1):
+    if n % 15 == 0:
+        print('fizzbuzz')
+    elif n % 3 == 0:
+        print('fizz')
+    elif n % 5 == 0:
+        print('buzz')
+    else:
+        print(n)
